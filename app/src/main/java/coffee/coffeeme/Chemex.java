a1e523b58ec249f75c1a9d30ac791d23e4424cfd
joshua shin <jdshin@cpp.edu>
2017-04-11 17:25:14 -0700

    first upload

---
+++ app/src/main/java/coffee/coffeeme/Chemex.java
@@ -0,0 +1,13 @@
+package coffee.coffeeme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chemex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chemex);
+    }
+}
