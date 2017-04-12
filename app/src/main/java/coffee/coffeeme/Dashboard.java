9eb147edb4aa005e6fd3e205bc57ec3b27bc8cd3
joshua shin <jdshin@cpp.edu>
2017-04-11 19:47:52 -0700

    update

--- app/src/main/java/coffee/coffeeme/Dashboard.java
+++ app/src/main/java/coffee/coffeeme/Dashboard.java
@@ -1,11 +1,13 @@
 package coffee.coffeeme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -16,14 +18,15 @@ public class Dashboard extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button btn = (Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(Dashboard.this, Chemex.class));
             }
         });
+
+
     }
 
 }
--- app/src/main/java/coffee/coffeeme/MainActivity.java
+++ app/src/main/java/coffee/coffeeme/MainActivity.java
@@ -1,7 +1,13 @@
 package coffee.coffeeme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.ViewGroup;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Dashboard.class));
+            }
+        });
+
+
+
 
     }
 
