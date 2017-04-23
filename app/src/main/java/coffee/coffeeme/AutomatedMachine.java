bbee65ad87a0a7441f4a6d2b69238631ce1ca4a0
joshua shin <jdshin@cpp.edu>
2017-04-23 15:56:17 -0700

    update

---
+++ app/src/main/java/coffee/coffeeme/AutomatedMachine.java
@@ -0,0 +1,13 @@
+package coffee.coffeeme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AutomatedMachine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_automated_machine);
+    }
+}
